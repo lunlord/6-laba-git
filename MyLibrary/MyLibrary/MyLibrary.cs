df1f366b2093763f81c71da5b16d9125091d3273
Ivan <vana_lalalala@mail.ru>
2022-01-10 11:13:48 +0300

    Добавлен класс MyLibrary, который содержит методы калькулятора

---
+++ MyLibrary/MyLibrary/MyLibrary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Project
+{
+    public static class MyLibrary
+    {
+        public static double Add(double firstValue, double secondValue)
+        {
+            return firstValue + secondValue;
+        }
+
+        public static double Substract(double firstValue, double secondValue)
+        {
+            return firstValue - secondValue;
+        }
+
+        public static double Myltiply(double firstValue, double secondValue)
+        {
+            return firstValue * secondValue;
+        }
+
+        public static double Divide(double firstValue, double secondValue)
+        {
+            return firstValue / secondValue;
+        }
+    }
+}
